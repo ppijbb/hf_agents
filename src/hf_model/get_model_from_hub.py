93f7ec16c7ef3b9d3c16ebbb02a88579da1486d5
ppijbb <ppijbb@naver.com>
2024-09-30 07:06:46 +0000

    refactor: Add model download from Gunulhona/Gemma-Ko-Merge-PEFT repository

--- src/hf_model/get_model_from_hub.py
+++ src/hf_model/get_model_from_hub.py
@@ -7,3 +7,11 @@ snapshot_download(
     local_dir="sample", 
     local_dir_use_symlinks=False, 
     revision="main")
+
+model_id = "Gunulhona/Gemma-Ko-Merge-PEFT"
+
+snapshot_download(
+    repo_id=model_id, 
+    local_dir="sample", 
+    local_dir_use_symlinks=False, 
+    revision="main")
