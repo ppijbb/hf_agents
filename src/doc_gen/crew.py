cb190c018bb28e1245d128c6ab4cd54a79a4ce81
ppijbb <ppijbb@naver.com>
2024-09-30 01:55:26 +0000

    refactor: Update .gitignore to include dialogue.txt and outputs/ directory

--- src/doc_gen/crew.py
+++ src/doc_gen/crew.py
@@ -99,17 +99,17 @@ class DocGenCrew():
 			process=Process.sequential,
 			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 			verbose=True,
-			planning=True,
+			# planning=True,
 			share_crew=True,
-			planning_llm=local_llm1,
-			manager_agent=Agent(
-				role='작업 매니저',
-				goal='전체 작엄 매니지먼트 전문가',
-				backstory="""
-				크루들 간의 원활한 업무가 진행되도록 관리, 감독합니다.
-				""",
-				verbose=True,
-				allow_delegation=True,
-				llm=local_llm1,
-			)
+			# planning_llm=local_llm2,
+			# manager_agent=Agent(
+			# 	role='작업 매니저',
+			# 	goal='전체 작엄 매니지먼트 전문가',
+			# 	backstory="""
+			# 	크루들 간의 원활한 업무가 진행되도록 관리, 감독합니다.
+			# 	""",
+			# 	verbose=True,
+			# 	allow_delegation=True,
+			# 	llm=local_llm2,
+			# )
 		)
