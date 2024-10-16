04ca29c2c9703537584da7d6cd4a405d604d498e
ppijbb <ppijbb@naver.com>
2024-10-16 01:59:31 +0000

    refactor: Update llama.cpp and ollama_model scripts
    - Update the subproject commit in llama.cpp from 13dca2a to 755a9b2
    - Change the MODEL_NAME in ollama_model script from Gemma-Med-Merge to Gemma-Med-Ko
    - Add LLAMA_CUBLAS=1 flag to the make command in setup_env script

--- src/hf_model/get_model_from_hub.py
+++ src/hf_model/get_model_from_hub.py
@@ -1,10 +1,10 @@
 from huggingface_hub import snapshot_download
 
-model_id = "Gunulhona/Gemma-Med-Merge"
+model_id = "Gunulhona/Gemma-Ko-Merge"
 
 snapshot_download(
     repo_id=model_id, 
-    local_dir="sample/med", 
+    local_dir="sample/ko", 
     local_dir_use_symlinks=False, 
     revision="main")
 
